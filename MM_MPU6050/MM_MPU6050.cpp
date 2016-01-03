2fe47872f0fd8f23189386b6a6771751b189218d
WinMic <l.everquest@gmx.de>
2016-01-03 11:13:43 +0100

    Loop und Setup aus der Library auskommentiert -zum Testen noch vorhanden-

--- MM_MPU6050/MM_MPU6050.cpp
+++ MM_MPU6050/MM_MPU6050.cpp
@@ -49,49 +49,76 @@ void MPUData(int16_t* p_ax, int16_t* p_ay, int16_t* p_az, int16_t* p_gx, int16_t
 	accelgyro.getMotion6(p_ax, p_ay, p_az, p_gx, p_gy, p_gz);
 }
 
+int MPUTemp()
+{	//holt sich 16 bit im 2er Kompliment format
+	int16_t smallInt = accelgyro.getTemperature();
 
 
+	const int negative = (smallInt & (1 << 17)) != 0;
+	int nativeInt;
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-boolean Debug = true;
+	if (negative)
+	  nativeInt = smallInt | ~((1 << 18) - 1);
+	else
+	  nativeInt = smallInt;
 
-void setup()
-{
-	Serial.begin(115200);
-	Wire.begin();
-
-
-
-	int Status = -1;
-
-	Status = MPUInit(Debug);
-
-	if (Status != MM_SUCCESS)
-	{
-		while(1)
-		{
-
-		}
-	}
-
+	return nativeInt;
 }
-void loop()
-{
-	MPUData(&ax, &ay, &az, &gx, &gy, &gz);
-	SerialPrint("Ax: ",Debug);
-	SerialPrintln((String)ax, Debug);
-	SerialPrint("Ay: ",Debug);
-	SerialPrintln((String)ay, Debug);
-	SerialPrint("Az: ",Debug);
-	SerialPrintln((String)az, Debug);
-	SerialPrint("Gx: ",Debug);
-	SerialPrintln((String)gx, Debug);
-	SerialPrint("Gy: ",Debug);
-	SerialPrintln((String)gy, Debug);
-	SerialPrint("Gz: ",Debug);
-	SerialPrintln((String)gz, Debug);
 
-	Sleep(1000);
 
-}
+
+
+//int16_t ax, ay, az;
+//int16_t gx, gy, gz;
+//boolean Debug = true;
+
+//void setup()
+//{
+//	Serial.begin(115200);
+//	Wire.begin();
+//
+//
+//
+//	int Status = -1;
+//
+//	Status = MPUInit(Debug);
+//
+//
+//	if (Status != MM_SUCCESS)
+//	{
+//		while(1)
+//		{
+//
+//		}
+//	}
+//
+//}
+//void loop()
+//{
+//	MPUData(&ax, &ay, &az, &gx, &gy, &gz);
+//
+//	float beschX = ax/16384.0;
+//	float beschY = ay/16384.0;
+//	float beschZ = az/16384.0;
+//
+//
+//	SerialPrint("Ax: ",Debug);
+//	SerialPrintln((String)beschX, Debug);
+//	SerialPrint("Ay: ",Debug);
+//	SerialPrintln((String)beschY, Debug);
+//	SerialPrint("Az: ",Debug);
+//	SerialPrintln((String)beschZ, Debug);
+//
+////	SerialPrint("Gx: ",Debug);
+////	SerialPrintln((String)gx, Debug);
+////	SerialPrint("Gy: ",Debug);
+////	SerialPrintln((String)gy, Debug);
+////	SerialPrint("Gz: ",Debug);
+////	SerialPrintln((String)gz, Debug);
+////
+////	short temp = MPUTemp();
+////	SerialPrintln((String)temp, Debug);
+//
+//	Sleep(1000);
+//
+//}
