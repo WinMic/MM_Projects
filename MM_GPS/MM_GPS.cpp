b5223315c8e4db5d937d31f60608ee4197062a2f
WinMic <l.everquest@gmx.de>
2016-11-22 15:53:29 +0100

    Library für das GPS Modul geschrieben.
    funktioniert soweit,nur müssen noch diverse aufräumarbeiten vorgenommen werden

---
+++ MM_GPS/MM_GPS.cpp
@@ -0,0 +1,140 @@
+// Do not remove the include below
+#include "MM_GPS.h"
+
+
+Adafruit_GPS GPS(&mySerial);
+
+//GPS wird ohne Interrupts verwendet
+boolean usingInterrupt = false;
+
+
+//Später Init
+void initGPS()
+{
+	  //Die TX und RX verbindung zwischen Arduino und GPS mit BAUD 9600
+	  GPS.begin(9600);
+	  mySerial.begin(9600);
+
+	  //Wir wollen RMC (recommended minimum) und GGA (fix data) inklusive der Höhe wissen
+	  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+
+	  /*
+	   * UpdateRaten:
+	   * PMTK_SET_NMEA_UPDATE_1HZ	~1Hz
+	   * PMTK_SET_NMEA_UPDATE_5HZ	~5Hz
+	   * PMTK_SET_NMEA_UPDATE_10HZ	~10Hz
+	   */
+	  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+
+	  //Antennenstatus bei abfrage anfordern
+	  GPS.sendCommand(PGCMD_ANTENNA);
+
+	  //kurz warten bis das GPS-Modul alles verdaut hat
+	  delay(1000);
+
+	  //FirmwareVersion abfragen
+	  mySerial.println(PMTK_Q_RELEASE);
+}
+
+
+//Später hole Daten
+//uint32_t timer = millis(); TODO: Timer löschen
+
+GPSValues readGPS()
+{
+	GPSValues myGPSData;
+	GPS.read();
+
+	// if a sentence is received, we can check the checksum, parse it...
+	  if (GPS.newNMEAreceived()) {
+	    // a tricky thing here is if we print the NMEA sentence, or data
+	    // we end up not listening and catching other sentences!
+	    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+	    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+
+	    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+	    	myGPSData.status = MM_GPS_ERROS;  // we can fail to parse a sentence in which case we should just wait for another
+	  }
+
+	  // if millis() or timer wraps around, we'll just reset it
+//	  if (timer > millis())  timer = millis(); TODO: Timer löschen
+
+	  // approximately every 2 seconds or so, print out the current stats
+//	  if (millis() - timer > 2000) TODO: Timer löschen
+	  {
+//	    timer = millis(); // reset the timer TODO: Timer löschen
+
+
+	    myGPSData.hour = (int)GPS.hour;
+//	    Serial.print(GPS.hour, DEC); Serial.print(':');
+
+	    myGPSData.minute = (int)GPS.minute;
+//	    Serial.print(GPS.minute, DEC); Serial.print(':');
+
+	    myGPSData.seconds = (int)GPS.seconds;
+//	    Serial.print(GPS.seconds, DEC); Serial.print('.');
+
+	    myGPSData.milliseconds = (int)GPS.milliseconds;
+//	    Serial.println(GPS.milliseconds);
+
+	    myGPSData.day = (int)GPS.day;
+//	    Serial.print(GPS.day, DEC); Serial.print('/');
+
+	    myGPSData.month = (int)GPS.month;
+//	    Serial.print(GPS.month, DEC); Serial.print("/20");
+
+	    myGPSData.year = (int)GPS.year;
+//	    Serial.println(GPS.year, DEC);
+
+	    myGPSData.fix = GPS.fix;
+//	    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+
+	    myGPSData.fixquality = (int)GPS.fixquality;
+//	    Serial.print(" Qualität: "); Serial.println((int)GPS.fixquality);
+
+		//
+	    if (GPS.fix)
+	    {
+	    	myGPSData.status = MM_SUCCESS;
+	    	myGPSData.latitude = GPS.latitude;
+//	        Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+
+	    	myGPSData.longitude = GPS.longitude;
+//	        Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+
+	    	//Von Knoten auf km/h
+	    	myGPSData.speed = GPS.speed*1.852;
+//			Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+
+	    	myGPSData.angle = GPS.angle;
+//			Serial.print("Angle: "); Serial.println(GPS.angle);
+
+	    	myGPSData.altitude = GPS.altitude;
+//			Serial.print("Altitude: "); Serial.println(GPS.altitude);
+
+	    	myGPSData.satellites = (int)GPS.satellites;
+//			Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+	    }
+
+//		  Serial.print(myGPSData.hour); Serial.print("h ");
+//		  Serial.print(myGPSData.minute); Serial.print("m ");
+//		  Serial.print(myGPSData.seconds); Serial.print("s ");
+//		  Serial.print(myGPSData.milliseconds); Serial.println("ms ");
+//		  Serial.print(myGPSData.day); Serial.print(".");
+//		  Serial.print(myGPSData.month); Serial.print(".");
+//		  Serial.println(myGPSData.year);
+//
+//		  Serial.print("fix: "); Serial.print(myGPSData.fix);
+//		  Serial.print(" qualety: "); Serial.println(myGPSData.fixquality);
+//
+//		  Serial.print("lat: ");  Serial.println(myGPSData.latitude);
+//		  Serial.print("lon: "); Serial.println(myGPSData.longitude);
+//		  Serial.print("Speed: "); Serial.println(myGPSData.speed);
+//		  Serial.print("angle: "); Serial.println(myGPSData.angle);
+//		  Serial.print("höhe: "); Serial.println(myGPSData.altitude);
+//		  Serial.print("satellites: "); Serial.println(myGPSData.satellites);
+//		  Serial.println("");
+	  }
+
+	  return myGPSData;
+}
---
+++ MM_GPS/MM_GPS.h
@@ -0,0 +1,35 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _MM_GPS_H_
+#define _MM_GPS_H_
+#include "Arduino.h"
+#include <Adafruit_GPS.h>
+#include <string.h>
+#include "MM_DEFINES"
+
+//   GPS TX to Arduino Due Serial1 RX pin 19
+//   GPS RX to Arduino Due Serial1 TX pin 18
+#define mySerial Serial1
+
+//GPS ECHO (RAW-Daten auf die Console schreiben)
+#define GPSECHO  false
+
+struct GPSValues
+{
+	boolean status = MM_UNDEFINED_ERROR;
+	int hour, minute, seconds, year, month, day, milliseconds, fixquality, satellites = 0;
+	// Floating point latitude and longitude value in degrees.
+	float latitude, longitude, altitude, speed, angle = 0.0;
+	boolean fix = false;
+};
+
+
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+
+
+//Do not add code below this line
+#endif /* _MM_GPS_H_ */
